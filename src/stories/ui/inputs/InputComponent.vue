2eb736333b85c401187dcd3cb57873c6640de521
KotoFeelGood72 <kotenkolove@yandex.ru>
2026-01-20 15:59:00 +0300

    Подключил инпуты

---
+++ src/stories/ui/inputs/InputComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { InputModel } from './model';
+import { computed } from 'vue';
+
+const props = defineProps<InputModel>();
+
+const inputProps = computed(() => {
+  // Для типа "file" нельзя устанавливать value/modelValue
+  if (props.type === 'file') {
+    const { modelValue, defaultValue, ...rest } = props;
+    return rest;
+  }
+  
+  return props;
+});
+
+</script>
+
+<template>
+  <B24Input
+    v-bind="inputProps"
+  />
+</template>
---
+++ src/stories/ui/inputs/model.ts
@@ -0,0 +1,50 @@
+import type { IconComponent, AvatarProps, AcceptableValue } from '@bitrix24/b24ui-nuxt';
+
+// Размеры инпутов
+export type InputSize = "xss" | "xs" | "sm" | "md" | "lg" | "xl"
+
+// Цветовая схема инпутов
+export type InputColor = "air-primary" | "air-primary-success" | "air-primary-alert" | "air-primary-warning" | "air-primary-copilot"
+
+// Цветовая схема для тегов
+export type TagColor = "air-primary" | "air-primary-success" | "air-primary-alert" | "air-primary-warning" | "air-primary-copilot" | "air-secondary" | "air-secondary-alert" | "air-secondary-accent" | "air-secondary-accent-1" | "air-secondary-accent-2" | "air-tertiary" | "air-selection"
+
+// Типы инпутов
+export type InputType = "number" | "image" | "text" | "button" | "search" | "time" | "color" | "checkbox" | "date" | "datetime-local" | "email" | "file" | "hidden" | "month" | "password" | "radio" | "range" | "reset" | "submit" | "tel" | "url" | "week" | string
+
+export interface InputModel {
+  as?: string;
+  id?: string;
+  name?: string;
+  type?: InputType;
+  placeholder?: string;
+  color?: InputColor;
+  size?: InputSize;
+  noPadding?: boolean;
+  noBorder?: boolean;
+  underline?: boolean;
+  rounded?: boolean;
+  required?: boolean;
+  autocomplete?: string;
+  autofocus?: boolean;
+  autofocusDelay?: number;
+  disabled?: boolean;
+  tag?: string;
+  tagColor?: TagColor;
+  highlight?: boolean;
+  modelValue?: AcceptableValue;
+  defaultValue?: AcceptableValue;
+  icon?: IconComponent;
+  avatar?: AvatarProps;
+  loading?: boolean;
+  trailing?: boolean;
+  trailingIcon?: IconComponent;
+  list?: string;
+  max?: string | number;
+  maxlength?: string | number;
+  min?: string | number;
+  minlength?: string | number;
+  pattern?: string;
+  readonly?: boolean | "true" | "false";
+  step?: string | number;
+}
