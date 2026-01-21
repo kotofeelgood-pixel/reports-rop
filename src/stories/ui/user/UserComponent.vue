1a3e1093144aa4b044deffd3c152958f5e4d9d20
KotoFeelGood72 <kotenkolove@yandex.ru>
2026-01-21 11:11:09 +0300

    ыы

---
+++ src/stories/ui/user/UserComponent.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import type { UserModel } from './model';
+
+const props = defineProps<UserModel>();
+
+defineSlots<{
+  avatar(): any;
+  name(): any;
+  description(): any;
+  default(): any;
+}>();
+
+</script>
+
+<template>
+  <B24User
+    v-bind="props"
+  >
+    <template
+      v-for="(_, slot) in $slots"
+      :key="slot"
+      #[slot]="scope"
+    >
+      <slot
+        :name="slot"
+        v-bind="scope"
+      />
+    </template>
+  </B24User>
+</template>
