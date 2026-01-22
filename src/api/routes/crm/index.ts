23aeb29c0b38f44512951ff6ccd4fed926576dc2
KotoFeelGood72 <kotenkolove@yandex.ru>
2026-01-22 12:41:09 +0300

    ss

--- src/api/routes/crm/index.ts
+++ src/api/routes/crm/index.ts
@@ -1,4 +1,4 @@
 import { requestWrapper } from '@/api/index'
 
-export const getDealCategories = () => requestWrapper<unknown, unknown>('crm.category.list', {}, (result) => result)
+export const getDealCategories = () => requestWrapper<unknown, unknown>('crm.category.list', {entityTypeId: 2}, (result) => result)
 
