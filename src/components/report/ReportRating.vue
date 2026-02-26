c0d8c390fa119679439977ead81caa688644c6d5
KotoFeelGood72 <kotenkolove@yandex.ru>
2026-02-26 12:28:13 +0300

    ss

--- src/components/report/ReportRating.vue
+++ src/components/report/ReportRating.vue
@@ -121,7 +121,7 @@ onMounted(() => {
     </template>
     <div :class="layoutType === 'rows' ? 'flex flex-col gap-4' : 'grid grid-cols-2 gap-4'">
       <B24Card variant="tinted">
-        <template #header>Совершенные звонки</template>
+        <template #header><p class="text-xs text-black">Совершенные звонки</p></template>
         <ul class="space-y-2 p-2">
           <li
             v-for="item in completedCalls"
@@ -153,7 +153,7 @@ onMounted(() => {
         </ul>
       </B24Card>
       <B24Card variant="tinted-warning">
-        <template #header>Пропущенные</template>
+        <template #header><p class="text-xs text-black">Пропущенные</p></template>
         <ul class="space-y-2 p-2">
           <li
             v-for="item in missedCalls"
